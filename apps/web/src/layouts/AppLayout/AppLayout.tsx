784fe5ad862006730ff96be76da2eaff82752f30
xavierroma <x.roma@pm.me>
2023-03-25 16:14:13 +0100

    feat: simplify request-flow

--- apps/web/src/layouts/AppLayout/AppLayout.tsx
+++ apps/web/src/layouts/AppLayout/AppLayout.tsx
@@ -9,10 +9,11 @@ interface AppLayoutProps extends PropsWithChildren {
 const AppLayout: FC<AppLayoutProps> = ({ children, breadcrumb }) => {
   return (
     <div className="flex flex-col flex-1 min-h-full">
-      <div className="p-4 flex justify-between items-center">
+      <div className="p-4 flex gap-5 flex-wrap flex-wrap-reverse justify-between items-center">
         <Breadcrumb hasLogo={true} items={breadcrumb} />
-
-        <ConnectButton />
+        <div className="ml-auto">
+          <ConnectButton />
+        </div>
       </div>
 
       <div className="flex flex-1 flex-col gap-4">{children}</div>
--- apps/web/src/layouts/CreateRequestLayout/CreateRequestLayout.tsx
+++ apps/web/src/layouts/CreateRequestLayout/CreateRequestLayout.tsx
@@ -15,29 +15,12 @@ export enum Steps {
 
 const CreateRequestLayout: FC<PropsWithChildren> = ({ children }) => {
   const { push, pathname } = useRouter();
-  const { description, amount } = useCreateRequestStore();
 
   const steps = useMemo(
     () => [
       {
-        title: "Amount",
-        description: amount ? `${amount} DAI` : "- DAI",
-        href: "/new/set-amount",
-        step: Steps.SetAmounts,
-      },
-      {
-        title: "Description",
-        href: "/new/set-description",
-        step: Steps.SetDescription,
-        // concatenated description
-        description:
-          description.length > 20
-            ? `${description.slice(0, 20)}...`
-            : description,
-      },
-      {
-        title: "Payers",
-        href: "/new/set-payers",
+        title: "New Request",
+        href: "/new/request",
         step: Steps.SetPayers,
       },
       {
@@ -46,28 +29,14 @@ const CreateRequestLayout: FC<PropsWithChildren> = ({ children }) => {
         step: Steps.SubmitRequest,
       },
     ],
-    [amount, description]
+    []
   );
 
-  const selectedStep = steps.find((step) => step.href === pathname);
+  const selectedStep = steps.find((step) => step.href === pathname) || "";
 
   return (
     <AppLayout breadcrumb={[{ label: "Create Request" }]}>
       <div className="max-w-2xl flex flex-col flex-1 w-full ml-auto mr-auto">
-        {/*   <div className="overflow-x-auto p-4">
-          <Stepper>
-            {steps.map(({ title, href, step, description }) => (
-              <StepperStep
-                key={step}
-                title={title}
-                href={href}
-                selected={selectedStep?.step === step}
-                description={description}
-              />
-            ))}
-          </Stepper>
-        </div> */}
-
         <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
 
         <div className="flex flex-col items-end justify-end gap-4 w-full p-4">
